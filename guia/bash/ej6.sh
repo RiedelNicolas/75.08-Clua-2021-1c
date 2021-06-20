6e4280c44a0fe3bdc1bf1c40c1959f0c085dbe1d
RiedelNicolas <nariedel99@gmail.com>
2021-06-20 01:21:57 -0300

    agrego

--- guia/bash/ej6.sh
+++ guia/bash/ej6.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 # PID
 
-num=1
+echo -e "Intente adivinar el process id de este script ..."
+read num
 
+i=0
 while [ ! $num -eq $$ ]; do
-    
+    if [ $num -lt $$ ]; then
+        echo -e " El numero ingresado es menor al PID"
+    else
+        echo -e "El numero ingresado es mayor al PID"
+    fi
+    echo -e "pruebe nuevamente"
+    read num
+    i=$((i++))
 done
+
+echo -e "Adivinaste el Process ID, en $i"
